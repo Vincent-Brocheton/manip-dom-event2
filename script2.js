52d6ca23d1cae64122d9f17092da8e8a98b91990
Vincent.Brocheton <vinc.brocheton@gmail.com>
2020-12-02 11:20:12 +0100

    first commit

---
+++ script2.js
@@ -0,0 +1,21 @@
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let genre = document.getElementById("sexe").value.toUpperCase();
+    let age = Number(document.getElementById("age").value);
+
+    if (genre === "H" || genre === "HOME") {
+        genre = "HOMME";
+    }
+    if (genre === "F" || genre === "FEME") {
+        genre = "FEMME";
+    }
+
+    if ((genre === "HOMME" && age >= 20) || (genre === "FEMME" && age >= 18 && age <= 35)) {
+        document.getElementById("result").textContent = "Vous devez payer l'impôts";
+    } else {
+        document.getElementById("result").textContent = "Vous avez de la chance";
+
+    }
+})
